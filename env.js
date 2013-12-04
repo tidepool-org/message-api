0e4a19f372aa9e95aff4827961ae72c169a62efe
Jamie Bate <jamie@tidepool.org>
2013-12-05 06:42:46 +1300

    remove unused commandline arg

--- env.js
+++ env.js
@@ -3,7 +3,7 @@ module.exports = (function(){
     var env = {};
 
     // The port for the server to listen on.
-    env.port = process.env.PORT || process.argv[3] || 3002;
+    env.port = process.env.PORT || 3002;
 
     //connection to mongo
     env.mongodb_connection_string = process.env.MONGO_CONNECTION_STRING || null;
