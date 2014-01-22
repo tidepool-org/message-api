82e4d15cafe459000693f5943e1acf2a25663b75
Jamie <jamie@tidepool.org>
2014-01-22 16:21:39 +1300

    save messages with message.parentmessage field being set

--- test/messages_api_integration_tests.js
+++ test/messages_api_integration_tests.js
@@ -17,10 +17,10 @@
 /* jshint -W079 *//* jshint -W098 */
 var should = require('chai').should(),
 /* jshint +W079 *//* jshint +W098 */
-    supertest = require('supertest'),
     mongojs = require('mongojs'),
     messagesToSave = require('./helpers/testMessagesData').relatedSet,
     testingHelper = require('./helpers/testingHelper')({integrationTest:true}),
+    supertest = require('supertest')(testingHelper.serviceEndpoint()),
     testDbInstance,
     crud;
 
@@ -39,11 +39,24 @@ describe('message API', function() {
 
         testDbInstance.messages.remove();
 
-        //set to be with the service endpoint
-        supertest = supertest(testingHelper.serviceEndpoint());
+        //save the first which will be the parent message
+        var isFirst = true,
+        parentMessageFromMongo;
+
 
         messagesToSave.forEach(function(message) {
-            testDbInstance.messages.save(message);
+
+            if(isFirst == false) {
+                message.parentmessage = parentMessageFromMongo._id;
+            }
+
+            testDbInstance.messages.save(message,function(err, doc){
+                if(doc && isFirst){
+                    parentMessageFromMongo = doc;
+                    console.log('saved ',parentMessageFromMongo._id);
+                    isFirst = false;
+                }
+            });
         });
 
     });
@@ -54,17 +67,17 @@ describe('message API', function() {
 
     describe('GET /api/message/:msgId', function() {
 
-        var testMessageId;
         var messageFromMongo;
 
         before(function(done){
-            
-            //Get id of existing message for tests 
+
+            // grab a message that has been saved already
             testDbInstance.messages.findOne({},function(err, doc) {
-                testMessageId = doc._id;
                 messageFromMongo = doc;
+                console.log('message to test wth ',messageFromMongo);
                 done();
             });
+
         });
 
         it('should not work without msgId parameter', function(done) {
@@ -78,7 +91,7 @@ describe('message API', function() {
 
         it('returns message for given id as JSON with content we expect', function(done) {
 
-            supertest.get('/api/message/read/'+testMessageId)
+            supertest.get('/api/message/read/'+messageFromMongo._id)
             .expect(200)
             .expect('Content-Type', 'application/json')
             .end(function(err, res) {
@@ -86,7 +99,7 @@ describe('message API', function() {
                 
                 var theMessage = res.body.message;
 
-                theMessage.id.should.equal(String(testMessageId));
+                theMessage.id.should.equal(String(messageFromMongo._id));
                 theMessage.parentmessage.should.equal(String(messageFromMongo.parentmessage));
                 theMessage.timestamp.should.equal(String(messageFromMongo.timestamp));
                 theMessage.groupid.should.equal(String(messageFromMongo.groupid));
@@ -101,7 +114,7 @@ describe('message API', function() {
 
             var messageFields = ['id', 'parentmessage', 'userid','groupid', 'timestamp', 'messagetext'];
 
-            supertest.get('/api/message/read/'+testMessageId)
+            supertest.get('/api/message/read/'+String(messageFromMongo._id))
             .expect(200)
             .expect('Content-Type', 'application/json')
             .end(function(err, res) {
